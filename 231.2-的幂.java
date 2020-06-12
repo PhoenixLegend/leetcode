d7833e9e286fd137535671e37984a17dbc209cbb
c_yanjia-001 <cjy@outlook.com>
2020-06-12 11:30:58 +0800

    231

--- 231.2-的幂.java
+++ 231.2-的幂.java
@@ -9,7 +9,7 @@
  * 
  * 解法1：移位运算：把二进制数进行左右移位。左移1位，扩大2倍；右移1位，缩小2倍。 return (n>0) && (1<<30) % n == 0; 1<<30为int最大的2的整数幂
  * 解法2：&运算，同1则1。 return (n > 0) && (n & -n) == n; 负数取补码，取反末尾加1
- * 解法3：递归除以2，取余数是否为1
+ * 解法3：递归除以2，取余数是否为奇数
  */
 
 // @lc code=start
@@ -19,11 +19,13 @@ class Solution {
     }
 
     public boolean isPowerOfTwo2(int n) {
-        if(n==1) return true;
-        if(n==0) return false;
-        if(n%2!=0) return false;
-        return isPowerOfTwo(n/2);
-        }
+        if (n == 1)
+            return true;
+        if (n == 0)
+            return false;
+        if (n % 2 != 0)
+            return false;
+        return isPowerOfTwo(n / 2);
     }
 }
 // @lc code=end
